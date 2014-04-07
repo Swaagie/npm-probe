d2d24de15f74a87fcdaee2fdb378e5e5b25cf51e
Swaagie <info@martijnswaagman.nl>
2014-04-07 15:55:13 +0200

    [minor] drop ping modules and prepare tests

--- probes/ping.js
+++ probes/ping.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var ping = require('pinger')
-  , async = require('async');
+var exec = require('child_process').exec;
 
 //
 // Name of the probe.
@@ -12,7 +11,7 @@ exports.name = 'ping';
 // Specifications when the probe should be run. Ping will be ran every 30 seconds.
 //
 exports.spec = {
-  second: [0, 10, 20, 30, 40, 50]
+  second: [0, 30]
 };
 
 /**
@@ -23,6 +22,17 @@ exports.spec = {
  * @param {Function} done completion callback.
  * @api public
  */
-exports.execute = function (collector, endpoint, done) {
-  ping(endpoint.host, done);
+exports.execute = function execute(collector, endpoint, done) {
+  exec('ping -c5 -q ' + endpoint.host, function process(error, output) {
+    if (error) return done(error);
+
+    output = output.slice(output.indexOf('= ') + 2, -5).split('/');
+
+    done(null, {
+      minimum: +output[0],
+      average: +output[1],
+      maximum: +output[2],
+      deviation: +output[3]
+    });
+  });
 };
